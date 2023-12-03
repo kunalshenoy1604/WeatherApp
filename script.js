c586d8b7a22950b5e162073fa3e303e5442d8a14
Kunal Shenoy <shenoykunal1309@gmail.com>
2023-12-03 18:09:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let weather = {
         document.querySelector(".humidity").innerText = " Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = " Wind speed: " + speed + " km/hr";
         document.querySelector(".weather").classList.remove("loading")
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + description + "')"
     },
     
     search: function(){
